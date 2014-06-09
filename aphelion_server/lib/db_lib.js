600b6a4c11843983ba15bfb95d8e8ce8ca6545cb
Nate <natethepcspecialist@gmail.com>
2014-06-09 16:30:29 -0400

    updated for hosting24

--- aphelion_server/lib/db_lib.js
+++ aphelion_server/lib/db_lib.js
@@ -14,10 +14,10 @@ function bit_rol(a,b){return a<<b|a>>>32-b}function safe_add(a,b){var c=(a&65535
 var mysql_client = require('mysql').createClient();
 
 mysql_client.host = 'localhost';
-mysql_client.user = 'b56cb47dfb38a5';//'root';
-mysql_client.password = 'e1757965';//'root';
+mysql_client.user = 'aphelius_admin';//'b56cb47dfb38a5';//'root';
+mysql_client.password = 'DevMe77';//'e1757965';//'root';
 // mysql_client.database = 'aphelion';//'DB';
-mysql_client.database = 'heroku_7f0556c4ad1fb74';
+mysql_client.database = 'aphelius_aphelion';//'heroku_7f0556c4ad1fb74';
 
 // Create tables if doesn't exist
 mysql_client.query("SELECT 1 FROM accounts", function (e, r) {
